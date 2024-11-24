45e8ee73a59a065e3cc288eab58b5d8861cc3e14
Nick <karenartyom2016@gmail.com>
2024-11-24 13:01:42 -0800

    Implement Searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,8 @@ import { Genre } from "./useGenres";
         params: {
             genres: gameQuery.genre?.id, 
             platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText
         }},
         [gameQuery]);
 
